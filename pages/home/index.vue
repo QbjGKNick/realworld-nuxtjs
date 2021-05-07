0eb53cb63450d45822e4896fb32895b16812523d
GKNick <gknick@126.com>
2021-05-08 07:45:45 +0800

    feat(tag.js index.vue): 实现tag交互

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,138 +1,213 @@
 <template>
   <div class="home-page">
-
-  <div class="banner">
-    <div class="container">
-      <h1 class="logo-font">conduit</h1>
-      <p>A place to share your knowledge.</p>
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font">conduit</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
     </div>
-  </div>
 
-  <div class="container page">
-    <div class="row">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="feed-toggle">
+            <ul class="nav nav-pills outline-active">
+              <li v-if="user" class="nav-item">
+                <nuxt-link
+                  class="nav-link"
+                  :class="{
+                    active: tab === 'your_feed',
+                  }"
+                  exact
+                  :to="{
+                    name: 'home',
+                    query: {
+                      tab: 'your_feed',
+                    },
+                  }"
+                  >Your Feed</nuxt-link
+                >
+              </li>
+              <li class="nav-item">
+                <nuxt-link
+                  class="nav-link"
+                  :class="{
+                    active: tab === 'global_feed',
+                  }"
+                  exact
+                  :to="{
+                    name: 'home',
+                    query: {
+                      tab: 'global_feed',
+                    },
+                  }"
+                  >Global Feed</nuxt-link
+                >
+              </li>
+              <li v-if="tag" class="nav-item">
+                <nuxt-link
+                  class="nav-link"
+                  :class="{
+                    active: tab === 'tag'
+                  }"
+                  :to="{
+                    name: 'home',
+                    query: {
+                      tab: 'tag',
+                      tag: tag
+                    }
+                  }"
+                  >#{{ tag }}</nuxt-link
+                >
+              </li>
+            </ul>
+          </div>
 
-      <div class="col-md-9">
-        <div class="feed-toggle">
-          <ul class="nav nav-pills outline-active">
-            <li class="nav-item">
-              <a class="nav-link disabled" href="">Your Feed</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="">Global Feed</a>
-            </li>
-          </ul>
-        </div>
-
-        <div class="article-preview" v-for="article in articles" :key="article.slug">
-          <div class="article-meta">
-            <nuxt-link :to="{
-              name: 'profile',
-              params: {
-                username: article.author.username
-              }
-            }">
-              <img :src="article.author.image" alt="">
-            </nuxt-link>
-            <div class="info">
-              <nuxt-link class="author" :to="{
-                name: 'profile',
-                params: {
-                  username: article.author.username
-                }
-              }">
-                {{ article.author.username }}
+          <div
+            class="article-preview"
+            v-for="article in articles"
+            :key="article.slug"
+          >
+            <div class="article-meta">
+              <nuxt-link
+                :to="{
+                  name: 'profile',
+                  params: {
+                    username: article.author.username,
+                  },
+                }"
+              >
+                <img :src="article.author.image" alt="" />
               </nuxt-link>
-              <span class="date">{{ article.createdAt }}</span>
+              <div class="info">
+                <nuxt-link
+                  class="author"
+                  :to="{
+                    name: 'profile',
+                    params: {
+                      username: article.author.username,
+                    },
+                  }"
+                >
+                  {{ article.author.username }}
+                </nuxt-link>
+                <span class="date">{{ article.createdAt }}</span>
+              </div>
+              <button
+                class="btn btn-outline-primary btn-sm pull-xs-right"
+                :class="{
+                  active: article.favorited,
+                }"
+              >
+                <i class="ion-heart"></i> {{ article.favoritesCount }}
+              </button>
             </div>
-            <button class="btn btn-outline-primary btn-sm pull-xs-right" :class="{
-              active: article.favorited
-            }">
-              <i class="ion-heart"></i> {{ article.favoritesCount }}
-            </button>
-          </div>
-          <nuxt-link class="preview-link" :to="{
-            name: 'article',
-            params: {
-              slug: article.slug
-            }
-          }">
-            <h1>{{ article.title }}</h1>
-            <p>{{ article.description }}</p>
-            <span>Read more...</span>
-          </nuxt-link>
-        </div>
-
-        <!-- 分页列表 -->
-        <nav>
-          <ul class="pagination">
-            <li class="page-item"
-              :class="{
-                active: item === page
+            <nuxt-link
+              class="preview-link"
+              :to="{
+                name: 'article',
+                params: {
+                  slug: article.slug,
+                },
               }"
-              v-for="item in totalPage"
-              :key="item"
             >
-              <nuxt-link :to="{
-                name: 'home',
-                query: {
-                  page: item
-                }
-              }" class="page-link">{{item}}</nuxt-link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+              <h1>{{ article.title }}</h1>
+              <p>{{ article.description }}</p>
+              <span>Read more...</span>
+            </nuxt-link>
+          </div>
 
-      <div class="col-md-3">
-        <div class="sidebar">
-          <p>Popular Tags</p>
+          <!-- 分页列表 -->
+          <nav>
+            <ul class="pagination">
+              <li
+                class="page-item"
+                :class="{
+                  active: item === page,
+                }"
+                v-for="item in totalPage"
+                :key="item"
+              >
+                <nuxt-link
+                  :to="{
+                    name: 'home',
+                    query: {
+                      page: item,
+                      tag: $route.query.tag,
+                    },
+                  }"
+                  class="page-link"
+                  >{{ item }}</nuxt-link
+                >
+              </li>
+            </ul>
+          </nav>
+        </div>
 
-          <div class="tag-list">
-            <a href="" class="tag-pill tag-default">programming</a>
-            <a href="" class="tag-pill tag-default">javascript</a>
-            <a href="" class="tag-pill tag-default">emberjs</a>
-            <a href="" class="tag-pill tag-default">angularjs</a>
-            <a href="" class="tag-pill tag-default">react</a>
-            <a href="" class="tag-pill tag-default">mean</a>
-            <a href="" class="tag-pill tag-default">node</a>
-            <a href="" class="tag-pill tag-default">rails</a>
+        <div class="col-md-3">
+          <div class="sidebar">
+            <p>Popular Tags</p>
+
+            <div class="tag-list">
+              <nuxt-link
+                :to="{
+                  name: 'home',
+                  query: {
+                    tab: 'tag',
+                    tag: item
+                  },
+                }"
+                class="tag-pill tag-default"
+                v-for="item in tags"
+                :key="item"
+                >{{ item }}</nuxt-link
+              >
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   </div>
-
-</div>
 </template>
 
 <script>
 import { getArticles } from "@/api/article"
+import { getTags } from "@/api/tag"
+import { mapState } from "vuex"
 export default {
-    name: 'HomeIndex',
-    async asyncData({ query }) {
-      const page = Number.parseInt(query.page || 1)
-      const limit = 20
-      const { data } = await getArticles({
+  name: "HomeIndex",
+  async asyncData({ query }) {
+    const page = Number.parseInt(query.page || 1)
+    const limit = 20
+    const { tag, tab } = query
+    const [articleRes, tagRes] = await Promise.all([
+      getArticles({
         limit,
-        offset: (page - 1) * limit
-      })
-      return {
-        articles: data.articles,
-        articlesCount: data.articlesCount,
-        limit,
-        page
-      }
-    },
-    watchQuery: ['page'],
-    computed: {
-      totalPage() {
-        return Math.ceil(this.articlesCount / this.limit)
-      }
+        offset: (page - 1) * limit,
+        tag,
+      }),
+      getTags(),
+    ])
+    const { articles, articlesCount } = articleRes.data
+    const { tags } = tagRes.data
+    return {
+      articles: articles,
+      articlesCount: articlesCount,
+      tags: tags,
+      limit,
+      page,
+      tag,
+      tab: tab || 'global_feed'
     }
+  },
+  watchQuery: ["page", "tag", "tab"],
+  computed: {
+    ...mapState(["user"]),
+    totalPage() {
+      return Math.ceil(this.articlesCount / this.limit)
+    },
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
