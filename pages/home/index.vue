52fa798b5dbd0d7d75f17a43a72b17ca34c94b4a
jiangqb <jiangqb@citycloud.com.cn>
2021-05-11 08:56:13 +0800

    feat(index.vue): 学习nuxtjs

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
   <div class="home-page">
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">nuxtjs learning</h1>
+        <h1 class="logo-font">nuxtjs 学习</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
